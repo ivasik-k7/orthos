96800e93ce6d5bd083dedeae0d52cf5211706361
ivasik-k7 <jeanlacroix@ukr.net>
2020-07-22 14:11:26 +0300

    js/styles suppush

--- js/bundle.js
+++ js/bundle.js
@@ -94,7 +94,31 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/scss/styles.scss\");\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__);\n\nconsole.log(\"hello \");\n\n//# sourceURL=webpack:///./src/js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/scss/styles.scss\");\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _modules_nameValidation__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/nameValidation */ \"./src/js/modules/nameValidation.js\");\n/* harmony import */ var _modules_mask__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/mask */ \"./src/js/modules/mask.js\");\n\n\n\nwindow.addEventListener(\"DOMContentLoaded\", function () {\n  Object(_modules_nameValidation__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(\"[data-reg-name]\");\n  Object(_modules_mask__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\"[data-reg-tel]\");\n});\n\n//# sourceURL=webpack:///./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/mask.js":
+/*!********************************!*\
+  !*** ./src/js/modules/mask.js ***!
+  \********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar mask = function mask(selector) {\n  var setCursor = function setCursor(pos, elem) {\n    elem.focus();\n\n    if (elem.setSelectionRange) {\n      elem.setSelectionRange(pos, pos);\n    } else if (elem.createTextRange) {\n      var range = elem.createTextRange();\n      range.collapse = true;\n      range.moveEnd(\"character\", pos);\n      range.moveStart(\"character\", pos);\n      range.select();\n    }\n  };\n\n  function makeMask(e) {\n    var matrix = \"+38 (___) ___ __ __\";\n    var i = 0;\n    var def = matrix.replace(/\\D/g, \"\");\n    var val = this.value.replace(/\\D/g, \"\");\n\n    if (def.length >= val.length) {\n      val = def;\n    }\n\n    this.value = matrix.replace(/./g, function (a) {\n      return /[_\\d]/.test(a) && i < val ? val.charAt(i++) : i >= val.length ? \"\" : a;\n    });\n\n    if (e.type === \"blur\") {\n      if (this.value.length == 2) {\n        this.value = \"\";\n      }\n    } else {\n      setCursor(this.value.length, this);\n    }\n  }\n\n  var inputsList = document.querySelectorAll(selector);\n  inputsList.forEach(function (inp) {\n    inp.addEventListener(\"input\", makeMask);\n    inp.addEventListener(\"focus\", makeMask);\n    inp.addEventListener(\"blur\", makeMask);\n  });\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (mask);\n\n//# sourceURL=webpack:///./src/js/modules/mask.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/nameValidation.js":
+/*!******************************************!*\
+  !*** ./src/js/modules/nameValidation.js ***!
+  \******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\nvar nameValitaion = function nameValitaion(selecotor) {\n  var nameInputs = document.querySelectorAll(selecotor);\n  var regex = /[^a-zA-Zа-яА-ЯёЁіІїЇ .]/g;\n\n  if (nameInputs.length) {\n    nameInputs.forEach(function (item) {\n      if (!regex.test(item.value)) {\n        item.addEventListener(\"keyup\", function () {\n          this.value = this.value.replace(regex, \"\");\n        });\n      } else {\n        return true;\n      }\n    });\n  }\n};\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (nameValitaion);\n\n//# sourceURL=webpack:///./src/js/modules/nameValidation.js?");
 
 /***/ }),
 
